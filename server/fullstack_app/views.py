e0c3f76d6f0fd54b5fb2e6a15d310168ce4e1500
domine29 <eric.dominguez29@gmail.com>
2022-12-01 11:06:47 -0600

    WIP

---
+++ server/fullstack_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+     homepage = open('static/index.html').read()
+     return HttpResponse(homepage)
